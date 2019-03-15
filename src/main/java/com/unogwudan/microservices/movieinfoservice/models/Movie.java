ce416cbb40383f1b9f1ca3cd7d9c65f7fb37ccaa
Unogwudan <unogwu.daniel@everyfarmer.farm>
2019-03-15 02:31:27 +0100

    Updated movie info service

---
+++ src/main/java/com/unogwudan/microservices/movieinfoservice/models/Movie.java
@@ -0,0 +1,46 @@
+package com.unogwudan.microservices.movieinfoservice.models;
+
+public class Movie {
+	
+	private String id;
+	private String name;
+	private String desc;
+
+	public Movie() {}
+	
+	public Movie(String id, String name, String desc) {
+		this.id = id;
+		this.name = name;
+		this.desc = desc;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+	@Override
+	public String toString() {
+		return "Movie [id=" + id + ", name=" + name + ", desc=" + desc + "]";
+	}
+
+}
